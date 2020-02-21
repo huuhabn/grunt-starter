d4b420db225013bcefe6d01b0b0ee029edcd1fa2
Huu Ha <ha.nh@kynaforkids.vn>
2020-02-21 18:58:31 +0700

    Update gitignore

---
+++ assets/js/grunt-starter.js
@@ -0,0 +1,12 @@
+/**
+grunt-starter - 1.1.0
+Huu Ha <huuhadev@gmail.com>
+http://posvn.net/
+Copyright (c) 2020 
+License: MIT
+*/
+// Make sure jQuery has been loaded
+if (typeof jQuery === 'undefined') {
+    throw new Error('grunt-starter requires jQuery')
+}
+
